6eb0943dc7353ad5f0d86605db1b9e33b25003d7
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-08 08:11:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,11 +13,12 @@ end
 def monopoly_with_second_tier
 	monopoly = { 
   :railroads=> {
-    :pieces=>4, 
+    :pieces=>4,
+    :rent_in_dollars=>{}, 
   :names=> {}
-  :rent_in_dollars=>{} 
   }
-  } 
+  }
+  
 end
 
 def monopoly_with_third_tier
